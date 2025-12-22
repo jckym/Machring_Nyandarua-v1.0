126f83912da58bc78e7817a7e44a0ca329740ab9
jckym <jckym001@gmail.com>
2025-12-22 13:41:01 +0300

    Update NavLink.tsx

--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -1,28 +1,35 @@
-import { NavLink as RouterNavLink, NavLinkProps } from "react-router-dom";
-import { forwardRef } from "react";
-import { cn } from "@/lib/utils";
+// src/components/ui/NavLink.tsx
+import React, { forwardRef } from 'react';
+import { NavLink as RouterNavLink, NavLinkProps } from 'react-router-dom';
+import { cn } from '@/lib/utils';
 
-interface NavLinkCompatProps extends Omit<NavLinkProps, "className"> {
-  className?: string;
+interface NavLinkCompatProps extends Omit<NavLinkProps, 'className'> {
+  className?: string | ((args: { isActive: boolean; isPending: boolean }) => string);
   activeClassName?: string;
   pendingClassName?: string;
 }
 
 const NavLink = forwardRef<HTMLAnchorElement, NavLinkCompatProps>(
-  ({ className, activeClassName, pendingClassName, to, ...props }, ref) => {
+  ({ className, activeClassName, pendingClassName, ...props }, ref) => {
     return (
       <RouterNavLink
         ref={ref}
-        to={to}
-        className={({ isActive, isPending }) =>
-          cn(className, isActive && activeClassName, isPending && pendingClassName)
-        }
         {...props}
+        className={({ isActive, isPending }) => {
+          const baseClass = typeof className === 'function' 
+            ? className({ isActive, isPending }) 
+            : className;
+
+          const activeClass = isActive ? activeClassName : '';
+          const pendingClass = isPending ? pendingClassName : '';
+
+          return cn(baseClass, activeClass, pendingClass);
+        }}
       />
     );
-  },
+  }
 );
 
-NavLink.displayName = "NavLink";
+NavLink.displayName = 'NavLink';
 
 export { NavLink };
